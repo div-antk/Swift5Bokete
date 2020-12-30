d558512e7064e2b4f6b5a3e283aad4438d79d53b
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-30 11:57:07 +0900

    APIキーをURLに設定

--- Swift5Bokete/ViewController.swift
+++ Swift5Bokete/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
+import Photos
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var odaiImageView: UIImageView!
+  @IBOutlet weak var commentTextView: UITextView!
+  @IBOutlet weak var searchTextView: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    commentTextView.layer.cornerRadius = 20.0
+    PHPhotoLibrary.requestAuthorization { (status) in
+      switch(status) {
+      case .authorized: break
+      case .denied: break
+      case .notDetermined: break
+      case .restricted: break
+        
+      }
+    }
+    
+    // 検索ワードの値を元に画像を持ってくる
+    // pixabay.com
+    
+    func geyImages(keyword:String) {
+      // APIKEY 19683077-8a212c49b30399ac1ec759a27
+      let url = "https://pixabay.com/api/?key=19683077-8a212c49b30399ac1ec759a27&q=\(keyword)"
+      
+      // Alamofireを使ってhttpリクエストを投げる
+      // 値が返ってきて、それをJSON解析を行う
+      // それをImageViewに貼り付ける
+      
+      AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+        
+        switch response.result {
+        case .success:
+          let json:JSON = JSON()
+        case .failure(let error):
+          print(error);
+        }
+      }
+    }
   }
-
-
-}
 
