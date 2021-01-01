45475986ed944dfcf8633411d12027c063248245
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-01 21:42:55 +0900

    使用許可の修正。アプリの完成

--- Swift5Bokete/ViewController.swift
+++ Swift5Bokete/ViewController.swift
@@ -23,85 +23,95 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
     commentTextView.layer.cornerRadius = 20.0
+    
     PHPhotoLibrary.requestAuthorization { (status) in
       switch(status) {
-      case .authorized: break
-      case .denied: break
-      case .notDetermined: break
-      case .restricted: break
+      case.authorized:
+        print("許可")
+      case .notDetermined:
+        print("不許可")
+      case .restricted:
+        print("制限付き")
         
+      case .denied:
+        print("否認")
+        
+      case .limited:
+        print("限定")
+        
+      @unknown default:
+        break
       }
     }
   }
+  
+  // 検索ワードの値を元に画像を持ってくる
+  // pixabay.com
+  
+  func getImages(keyword:String) {
+    // APIKEY 19683077-8a212c49b30399ac1ec759a27
+    let url = "https://pixabay.com/api/?key=19683077-8a212c49b30399ac1ec759a27&q=\(keyword)"
     
-    
-    // 検索ワードの値を元に画像を持ってくる
-    // pixabay.com
-    
-    func getImages(keyword:String) {
-      // APIKEY 19683077-8a212c49b30399ac1ec759a27
-      let url = "https://pixabay.com/api/?key=19683077-8a212c49b30399ac1ec759a27&q=\(keyword)"
+    // Alamofireを使ってhttpリクエストを投げる
+    // 値が返ってきて、それをJSON解析を行う
+    // それをImageViewに貼り付ける
+    AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
       
-      // Alamofireを使ってhttpリクエストを投げる
-      // 値が返ってきて、それをJSON解析を行う
-      // それをImageViewに貼り付ける
-      AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+      switch response.result {
+      
+      case .success:
+        let json:JSON = JSON(response.data as Any)
+        var imageString = json["hits"][self.count]["webformatURL"].string
         
-        switch response.result {
-        
-        case .success:
-          let json:JSON = JSON(response.data as Any)
-          var imageString = json["hits"][self.count]["webformatURL"].string
+        if imageString == nil {
           
-          if imageString == nil {
-            
-            imageString = json["hits"][0]["webformatURL"].string
-            self .odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
-            
-          } else {
-            
-            self .odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
-          }
+          imageString = json["hits"][0]["webformatURL"].string
+          self .odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
           
-        case .failure(let error):
+        } else {
           
-          print(error);
+          self .odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
         }
-      }
-    }
-    
-    
-    @IBAction func nextOdai(_ sender: Any) {
-      count += 1
-      // 検索窓が空だった場合
-      if searchTextView.text == "" {
-        getImages(keyword: "funny")
         
-      } else {
-        getImages(keyword: searchTextView.text!)
-      }
-    }
-    
-    @IBAction func searchAction(_ sender: Any) {
-      
-      self.count = 0
-      if searchTextView.text == "" {
-        getImages(keyword: "funny")
+      case .failure(let error):
         
-      } else {
-        getImages(keyword: searchTextView.text!)
+        print(error);
       }
-    }
-    
-    
-    @IBAction func next(_ sender: Any) {
-      performSegue(withIdentifier: "next", sender: nil)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      let shareVC = segue.destination as? ShareViewController
-      
-      shareVC?.commentString = commentTextView.text
-      shareVC?.resultImage = odaiImageView.image!
     }
   }
+  
+  
+  @IBAction func nextOdai(_ sender: Any) {
+    count += 1
+    // 検索窓が空だった場合
+    if searchTextView.text == "" {
+      getImages(keyword: "funny")
+      
+    } else {
+      getImages(keyword: searchTextView.text!)
+    }
+  }
+  
+  @IBAction func searchAction(_ sender: Any) {
+    
+    self.count = 0
+    if searchTextView.text == "" {
+      getImages(keyword: "funny")
+      
+    } else {
+      getImages(keyword: searchTextView.text!)
+    }
+  }
+  
+  
+  @IBAction func next(_ sender: Any) {
+    performSegue(withIdentifier: "next", sender: nil)
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let shareVC = segue.destination as? ShareViewController
+    
+    shareVC?.commentString = commentTextView.text
+    shareVC?.resultImage = odaiImageView.image!
+  }
+}
