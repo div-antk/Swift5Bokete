8b88e80e33a703030b2cfc7c7bbba0e2163e1945
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-01 20:41:29 +0900

    画像がなかった場合の処理と次画面のコントローラ作成

---
+++ Swift5Bokete/ShareViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ShareViewController.swift
+//  Swift5Bokete
+//
+//  Created by Takuya Ando on 2021/01/01.
+//
+
+import UIKit
+
+class ShareViewController: UIViewController {
+
+    var resultImage = UIImage()
+    var commentString = String()
+    var screenShotImage = UIImage()
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Swift5Bokete/ViewController.swift
+++ Swift5Bokete/ViewController.swift
@@ -32,6 +32,7 @@ class ViewController: UIViewController {
         
       }
     }
+  }
     
     // 検索ワードの値を元に画像を持ってくる
     // pixabay.com
@@ -43,15 +44,23 @@ class ViewController: UIViewController {
       // Alamofireを使ってhttpリクエストを投げる
       // 値が返ってきて、それをJSON解析を行う
       // それをImageViewに貼り付ける
-      
       AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
         
         switch response.result {
         
         case .success:
           let json:JSON = JSON(response.data as Any)
-          let imageString = json["hits"][self.count]["webformatURL"].string
-          self .odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+          var imageString = json["hits"][self.count]["webformatURL"].string
+          
+          if imageString == nil {
+            
+            imageString = json["hits"][0]["webformatURL"].string
+            self .odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+            
+          } else {
+            
+            self .odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+          }
           
         case .failure(let error):
           
@@ -59,33 +68,39 @@ class ViewController: UIViewController {
         }
       }
     }
-    getImages(keyword: "funny")
-  }
-  
-  @IBAction func nextOdai(_ sender: Any) {
-    count += 1
-    // 検索窓が空だった場合
-    if searchTextView.text == "" {
-      getImages(keyword: "funny")
-
-    } else {
-      getImages(keyword: searchTextView.text!)
-    }
-  }
-  
-  @IBAction func searchAction(_ sender: Any) {
     
-    self.count = 0
-    if searchTextView.text == "" {
-      getImages(keyword: "funny")
-
-    } else {
-      getImages(keyword: searchTextView.text!)
+    
+    @IBAction func nextOdai(_ sender: Any) {
+      count += 1
+      // 検索窓が空だった場合
+      if searchTextView.text == "" {
+        getImages(keyword: "funny")
+        
+      } else {
+        getImages(keyword: searchTextView.text!)
+      }
+    }
+    
+    @IBAction func searchAction(_ sender: Any) {
+      
+      self.count = 0
+      if searchTextView.text == "" {
+        getImages(keyword: "funny")
+        
+      } else {
+        getImages(keyword: searchTextView.text!)
+      }
+    }
+    
+    
+    @IBAction func next(_ sender: Any) {
+      performSegue(withIdentifier: "next", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      let shareVC = segue.destination as? ShareViewController
+      
+      shareVC?.commentString = commentTextView.text
+      shareVC?.resultImage = odaiImageView.image!
     }
   }
-  
-  
-  @IBAction func next(_ sender: Any) {
-    performSegue(withIdentifier: next, sender: nil)
-  }
-}
